75359cb5b9526c2db2289b05450cabe9acf61731
Alexandra Wang <lewang@pivotal.io>
2018-10-30 14:14:27 -0700

    ci: Drop functions with dependencies not shipped to customers at end of ICW
    The sqldump that is generated from the icw_gporca_centos6 job can't be fully
    loaded into the test cluster for the pg_upgrade and gpexpand jobs because those
    clusters are missing some required libraries and extensions which would have
    been installed during ICW.  However all of the missing pieces are not compiled
    objects that we ship.  We do not expect a sqldump file to be restore thes
    compiled objects.  Therefore we are going to drop the objects from the database
    that depend on bits we don' ship.
    
    We are dropping any function that is not installed int $GPHOME/lib/posgresql
    ($libdir) and we are explicitly dropping functions that depend on:
    
    + $libdir/gp_replica_check
    + $libdir/gpformatter.so
    + $libdir/gpextprotocol.so
    + $libdir/gpcc_test
    + $libdir/gpcc_demo
    + $libdir/tabfunc_gppc_demo
    
    This does not mean that we aren't supporting upgrading this class of objects,
    note that they are still upgraded as a part of the smoke test at the end of
    ICW.  We also are leaving in many functions that depend on bits we ship, so
    downstream consumers of this data set are not loosing a class of objects to
    upgrade.  Also note these destructive changes to the database will only be
    applied after ICW succeeds and so shouldn't hamper debugging ICW in the future.
    
    Co-authored-by: Alexandra Wang <lewang@pivotal.io>
    Co-authored-by: Jim Doty <jdoty@pivotal.io>

--- concourse/scripts/dumpdb.bash
+++ concourse/scripts/dumpdb.bash
@@ -4,5 +4,6 @@ source /usr/local/greenplum-db-devel/greenplum_path.sh
 source /opt/gcc_env.sh
 source ./gpdb_src/gpAux/gpdemo/gpdemo-env.sh
 gpstart -a
+psql regression -f ./gpdb_src/concourse/scripts/drop_functions_with_dependencies_not_shipped_to_customers.sql
 pg_dumpall -f ./sqldump/dump.sql
 xz -z ./sqldump/dump.sql
