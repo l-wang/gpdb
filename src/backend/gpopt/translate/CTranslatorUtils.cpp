cloudberrydb/cloudberrydb

commit df3ce784285f7d9ceccfa761fd790ece65b65cde

Re-enable GPORCA support for GROUPTING SETS and ROLLUP
Grouping sets support in ORCA was disabled during the Postgres 9.5 merge
due to changes of the `Query` tree structure. Specifically, the
`groupClause` field was decoupled into two fields: `groupClause` and
`groupingSets`, where `groupClause` stores all SortGroupClauses and
`groupingSets` is designated to store grouping sets (GROUPING
SETS/ROLLUP/CUBE and empty sets).

This patch updates the GPORCA translator code to adapt the above change.

The updates includes:

1. Consider `groupingSets` along with `groupClause` during query
normalization for GROUP BY, HAVING and WINDOW FUNC. Eliminating DISTINCT
clause remains the same because a DISTINCT clause is only converted into
a simple GROUP BY clause.

2. Update CTranslatorQueryToDXL to utilize the `groupingSets` structure
to collect grouping sets.

Notes:
1. GROUPING FUNC is still not supported yet.
2. Same as 6X_STABLE, some queries that are supported in planner are
   still not supported in ORCA, including:
    a. Multiple grouping sets specifications
    b. Mixing simple GROUP BY and GROUPING SETS
    c. Nested grouping sets
    d. CUBE

Co-authored-by: Alexandra Wang <lewang@pivotal.io>

src/backend/gpopt/translate/CTranslatorUtils.cpp
@@ -1036,52 +1036,178 @@ CTranslatorUtils::GetGroupingColidArray(
 //---------------------------------------------------------------------------
 CBitSetArray *
 CTranslatorUtils::GetColumnAttnosForGroupBy(
-	CMemoryPool *mp, List *group_clause_list, ULONG num_cols,
+	CMemoryPool *mp, List *group_clause_list, List *grouping_set_list,
+	ULONG num_cols,
 	UlongToUlongMap *
 		group_col_pos,	// mapping of grouping col positions to SortGroupRef ids
 	CBitSet *group_cols	 // existing uniqueue grouping columns
 )
 {
-	GPOS_ASSERT(NULL != group_clause_list);
-	GPOS_ASSERT(0 < gpdb::ListLength(group_clause_list));
 	GPOS_ASSERT(NULL != group_col_pos);
 
-	Node *node = (Node *) LInitial(group_clause_list);
-
-	if (NULL == node || IsA(node, SortGroupClause))
+	if (NIL == grouping_set_list)
 	{
 		// simple group by
 		CBitSet *col_attnos = CreateAttnoSetForGroupingSet(
-			mp, group_clause_list, num_cols, group_col_pos, group_cols);
+			mp, group_clause_list, num_cols, group_col_pos, group_cols,
+			true /* use_group_clause */);
 		CBitSetArray *col_attnos_arr = GPOS_NEW(mp) CBitSetArray(mp);
 		col_attnos_arr->Append(col_attnos);
 		return col_attnos_arr;
 	}
 
-	GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLUnsupportedFeature,
-			   GPOS_WSZ_LIT("Group by clause"));
+	GPOS_ASSERT(0 < gpdb::ListLength(grouping_set_list));
 
-	return NULL;
+	if (1 < gpdb::ListLength(grouping_set_list))
+	{
+		GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLUnsupportedFeature,
+				   GPOS_WSZ_LIT("Multiple grouping sets specifications"));
+	}
+
+	Node *node = (Node *) LInitial(grouping_set_list);
+	GPOS_ASSERT(NULL != node && IsA(node, GroupingSet));
+	GroupingSet *grouping_set = (GroupingSet *) node;
+	CBitSetArray *col_attnos_arr = NULL;
+
+	switch (grouping_set->kind)
+	{
+		case GROUPING_SET_EMPTY:
+		{
+			col_attnos_arr = GPOS_NEW(mp) CBitSetArray(mp);
+			CBitSet *bset = GPOS_NEW(mp) CBitSet(mp);
+			col_attnos_arr->Append(bset);
+			break;
+		}
+		case GROUPING_SET_ROLLUP:
+		{
+			col_attnos_arr = CreateGroupingSetsForRollup(
+				mp, grouping_set, num_cols, group_cols, group_col_pos);
+			break;
+		}
+		case GROUPING_SET_CUBE:
+		{
+			GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLUnsupportedFeature,
+					   GPOS_WSZ_LIT("Cube"));
+		}
+		case GROUPING_SET_SETS:
+		{
+			col_attnos_arr = CreateGroupingSetsForSets(
+				mp, grouping_set, num_cols, group_cols, group_col_pos);
+			break;
+		}
+		case GROUPING_SET_SIMPLE:
+		default:
+		{
+			/* can't happen */
+			GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLError,
+					   GPOS_WSZ_LIT("Unrecognized grouping set kind"));
+		}
+	}
+
+	return col_attnos_arr;
 }
 
+//---------------------------------------------------------------------------
+//	@function:
+//		CTranslatorUtils::CreateGroupingSetsForSets
+//
+//	@doc:
+//		Construct a dynamic array of sets of column attnos for a grouping sets
+//		subclause
+//
+//---------------------------------------------------------------------------
+CBitSetArray *
+CTranslatorUtils::CreateGroupingSetsForSets(CMemoryPool *mp,
+											const GroupingSet *grouping_set,
+											ULONG num_cols, CBitSet *group_cols,
+											UlongToUlongMap *group_col_pos)
+{
+	GPOS_ASSERT(NULL != grouping_set);
+	GPOS_ASSERT(grouping_set->kind == GROUPING_SET_SETS);
+	CBitSetArray *col_attnos_arr = GPOS_NEW(mp) CBitSetArray(mp);
+
+	ListCell *cell = NULL;
+	ForEach(cell, grouping_set->content)
+	{
+		Node *n = (Node *) lfirst(cell);
+		GPOS_ASSERT(IsA(n, GroupingSet));
+		GroupingSet *gs_current = (GroupingSet *) n;
+
+		CBitSet *bset = NULL;
+		switch (gs_current->kind)
+		{
+			case GROUPING_SET_EMPTY:
+				bset = GPOS_NEW(mp) CBitSet(mp, num_cols);
+				break;
+			case GROUPING_SET_SIMPLE:
+				bset = CreateAttnoSetForGroupingSet(
+					mp, gs_current->content, num_cols, group_col_pos,
+					group_cols, false /* use_group_clause */);
+				break;
+			default:
+				GPOS_RAISE(ExmaDXL, ExmiQuery2DXLUnsupportedFeature,
+						   GPOS_WSZ_LIT("nested grouping set"));
+		}
+		col_attnos_arr->Append(bset);
+	}
+	return col_attnos_arr;
+}
+
+//---------------------------------------------------------------------------
+//	@function:
+//		CTranslatorUtils::CreateGroupingSetsForRollup
+//
+//	@doc:
+//		Construct a dynamic array of sets of column attnos for a rollup
+//
+//---------------------------------------------------------------------------
+CBitSetArray *
+CTranslatorUtils::CreateGroupingSetsForRollup(CMemoryPool *mp,
+											  const GroupingSet *grouping_set,
+											  ULONG num_cols,
+											  CBitSet *group_cols,
+											  UlongToUlongMap *group_col_pos)
+{
+	GPOS_ASSERT(NULL != grouping_set);
+	GPOS_ASSERT(grouping_set->kind == GROUPING_SET_ROLLUP);
+	CBitSetArray *col_attnos_arr = GPOS_NEW(mp) CBitSetArray(mp);
+	ListCell *lc = NULL;
+	CBitSet *current_result = GPOS_NEW(mp) CBitSet(mp);
+	ForEach(lc, grouping_set->content)
+	{
+		GroupingSet *gs_current = (GroupingSet *) lfirst(lc);
+		GPOS_ASSERT(gs_current->kind == GROUPING_SET_SIMPLE);
+
+		CBitSet *bset = CreateAttnoSetForGroupingSet(
+			mp, gs_current->content, num_cols, group_col_pos, group_cols,
+			false /* use_group_clause */);
+		current_result->Union(bset);
+		col_attnos_arr->Append(GPOS_NEW(mp) CBitSet(mp, *current_result));
+	}
+	// add an empty set
+	col_attnos_arr->Append(GPOS_NEW(mp) CBitSet(mp));
+	current_result->Release();
+	return col_attnos_arr;
+}
 
 //---------------------------------------------------------------------------
 //	@function:
 //		CTranslatorUtils::CreateAttnoSetForGroupingSet
 //
 //	@doc:
-//		Construct a set of column attnos corresponding to a grouping set
+//		Construct a set of column attnos corresponding to a single grouping set
+//		from either a simple GROUP BY or a list of grouping sets
 //
 //---------------------------------------------------------------------------
 CBitSet *
 CTranslatorUtils::CreateAttnoSetForGroupingSet(
 	CMemoryPool *mp, List *group_elems, ULONG num_cols,
 	UlongToUlongMap *
-		group_col_pos,	// mapping of grouping col positions to SortGroupRef ids,
-	CBitSet *group_cols	 // existing grouping columns
-)
+		group_col_pos,	// mapping of grouping col positions to SortGroupRef ids
+	CBitSet *group_cols,  // existing grouping columns
+	bool use_group_clause)
 {
-	GPOS_ASSERT(NULL != group_elems);
+	GPOS_ASSERT(NIL != group_elems);
 	GPOS_ASSERT(0 < gpdb::ListLength(group_elems));
 
 	CBitSet *bs = GPOS_NEW(mp) CBitSet(mp, num_cols);
@@ -1089,23 +1215,19 @@ CTranslatorUtils::CreateAttnoSetForGroupingSet(
 	ListCell *lc = NULL;
 	ForEach(lc, group_elems)
 	{
-		Node *elem_node = (Node *) lfirst(lc);
-
-		if (NULL == elem_node)
+		ULONG sort_group_ref;
+		if (use_group_clause)
 		{
-			continue;
+			Node *elem_node = (Node *) lfirst(lc);
+			GPOS_ASSERT(NULL != elem_node);
+			GPOS_ASSERT(IsA(elem_node, SortGroupClause));
+			sort_group_ref = ((SortGroupClause *) elem_node)->tleSortGroupRef;
 		}
-
-		if (!IsA(elem_node, SortGroupClause))
+		else
 		{
-			GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLUnsupportedFeature,
-					   GPOS_WSZ_LIT(
-						   "Mixing grouping sets with simple group by lists"));
+			sort_group_ref = lfirst_int(lc);
 		}
-
-		ULONG sort_group_ref = ((SortGroupClause *) elem_node)->tleSortGroupRef;
 		bs->ExchangeSet(sort_group_ref);
-
 		UpdateGrpColMapping(mp, group_col_pos, group_cols, sort_group_ref);
 	}
 

src/backend/gpopt/translate/CQueryMutators.cpp
@@ -47,7 +47,8 @@ using namespace gpmd;
 BOOL
 CQueryMutators::NeedsProjListNormalization(const Query *query)
 {
-	if (!query->hasAggs && NULL == query->groupClause)
+	if (!query->hasAggs && NULL == query->groupClause &&
+		NULL == query->groupingSets)
 	{
 		return false;
 	}
@@ -975,7 +976,8 @@ CQueryMutators::NormalizeHaving(CMemoryPool *mp, CMDAccessor *md_accessor,
 
 	ReassignSortClause(new_query, rte->subquery);
 
-	if (!rte->subquery->hasAggs && NIL == rte->subquery->groupClause)
+	if (!rte->subquery->hasAggs && NIL == rte->subquery->groupClause &&
+		NIL == rte->subquery->groupingSets)
 	{
 		// if the derived table has no grouping columns or aggregates then the
 		// subquery is equivalent to select XXXX FROM CONST-TABLE
@@ -1009,6 +1011,7 @@ CQueryMutators::NormalizeHaving(CMemoryPool *mp, CMDAccessor *md_accessor,
 		rte->subquery = new_subquery;
 		rte->subquery->jointree = MakeNode(FromExpr);
 		rte->subquery->groupClause = NIL;
+		rte->subquery->groupingSets = NIL;
 		rte->subquery->sortClause = NIL;
 		rte->subquery->windowClause = NIL;
 	}
@@ -1390,6 +1393,7 @@ CQueryMutators::NormalizeWindowProjList(CMemoryPool *mp,
 	// transformation, which separates GROUP BY from window functions
 	GPOS_ASSERT(NULL == original_query->distinctClause);
 	GPOS_ASSERT(NULL == original_query->groupClause);
+	GPOS_ASSERT(NULL == original_query->groupingSets);
 
 	// we do not fix target list of the derived table since we will be mutating it below
 	// to ensure that it does not have window functions

src/backend/gpopt/translate/CTranslatorQueryToDXL.cpp
@@ -131,12 +131,6 @@ CTranslatorQueryToDXL::CTranslatorQueryToDXL(
 				   GPOS_WSZ_LIT("View with WITH CHECK OPTION"));
 	}
 
-	// Grouping sets are not supported yet.
-	// GPDB_95_MERGE_FIXME: Support grouping sets in ORCA?
-	if (query->groupingSets)
-		GPOS_RAISE(gpdxl::ExmaDXL, gpdxl::ExmiQuery2DXLUnsupportedFeature,
-				   GPOS_WSZ_LIT("Grouping Sets"));
-
 	// Initialize the map that stores gpdb att to optimizer col mapping.
 	// If this is a subquery, make a copy of the parent's mapping, otherwise
 	// initialize a new, empty, mapping.
@@ -564,6 +558,7 @@ CTranslatorQueryToDXL::TranslateSelectQueryToDXL()
 	{
 		CDXLNode *dxlnode = TranslateFromExprToDXL(m_query->jointree);
 		GPOS_ASSERT(NULL == m_query->groupClause);
+		GPOS_ASSERT(NULL == m_query->groupingSets);
 		child_dxlnode = TranslateWindowToDXL(
 			dxlnode, m_query->targetList, m_query->windowClause,
 			m_query->sortClause, sort_group_attno_to_colid_mapping,
@@ -573,8 +568,8 @@ CTranslatorQueryToDXL::TranslateSelectQueryToDXL()
 	{
 		child_dxlnode = TranslateGroupingSets(
 			m_query->jointree, m_query->targetList, m_query->groupClause,
-			m_query->hasAggs, sort_group_attno_to_colid_mapping,
-			output_attno_to_colid_mapping);
+			m_query->groupingSets, m_query->hasAggs,
+			sort_group_attno_to_colid_mapping, output_attno_to_colid_mapping);
 	}
 
 	// translate limit clause
@@ -2173,13 +2168,14 @@ CTranslatorQueryToDXL::IsDuplicateDqaArg(List *dqa_list, Aggref *aggref)
 //---------------------------------------------------------------------------
 CDXLNode *
 CTranslatorQueryToDXL::TranslateGroupingSets(
-	FromExpr *from_expr, List *target_list, List *group_clause, BOOL has_aggs,
+	FromExpr *from_expr, List *target_list, List *group_clause,
+	List *grouping_set, BOOL has_aggs,
 	IntToUlongMap *sort_grpref_to_colid_mapping,
 	IntToUlongMap *output_attno_to_colid_mapping)
 {
 	const ULONG num_of_cols = gpdb::ListLength(target_list) + 1;
 
-	if (NULL == group_clause)
+	if (NULL == group_clause && NULL == grouping_set)
 	{
 		IntToUlongMap *child_attno_colid_mapping =
 			GPOS_NEW(m_mp) IntToUlongMap(m_mp);
@@ -2213,8 +2209,8 @@ CTranslatorQueryToDXL::TranslateGroupingSets(
 		GPOS_NEW(m_mp) UlongToUlongMap(m_mp);
 	CBitSet *unique_grp_cols_bitset = GPOS_NEW(m_mp) CBitSet(m_mp, num_of_cols);
 	CBitSetArray *bitset_array = CTranslatorUtils::GetColumnAttnosForGroupBy(
-		m_mp, group_clause, num_of_cols, grpcol_index_to_colid_mapping,
-		unique_grp_cols_bitset);
+		m_mp, group_clause, grouping_set, num_of_cols,
+		grpcol_index_to_colid_mapping, unique_grp_cols_bitset);
 
 	const ULONG num_of_grouping_sets = bitset_array->Size();
 
@@ -3852,7 +3848,8 @@ CTranslatorQueryToDXL::TranslateTargetListToDXLProject(
 	BOOL is_expand_aggref_expr)
 {
 	BOOL is_groupby =
-		(0 != gpdb::ListLength(m_query->groupClause) || m_query->hasAggs);
+		(0 != gpdb::ListLength(m_query->groupClause) ||
+		 0 != gpdb::ListLength(m_query->groupingSets) || m_query->hasAggs);
 
 	CDXLNode *project_list_dxlnode =
 		GPOS_NEW(m_mp) CDXLNode(m_mp, GPOS_NEW(m_mp) CDXLScalarProjList(m_mp));

src/backend/optimizer/plan/orca.c
@@ -507,7 +507,8 @@ transformGroupedWindows(Node *node, void *context)
 		/*
 		 * we are done if this query doesn't have both window functions and group by/aggregates
 		 */
-		if (!qry->hasWindowFuncs || !(qry->groupClause || qry->hasAggs))
+		if (!qry->hasWindowFuncs ||
+			!(qry->groupClause || qry->groupingSets || qry->hasAggs))
 			return (Node *) qry;
 
 		Query	   *subq;

src/include/gpopt/translate/CTranslatorQueryToDXL.h
@@ -230,7 +230,8 @@ private:
 	// translate a query with grouping sets
 	CDXLNode *TranslateGroupingSets(
 		FromExpr *from_expr, List *target_list, List *group_clause,
-		BOOL has_aggs, IntToUlongMap *phmiulSortGrpColsColId,
+		List *grouping_set, BOOL has_aggs,
+		IntToUlongMap *phmiulSortGrpColsColId,
 		IntToUlongMap *output_attno_to_colid_mapping);
 
 	// expand the grouping sets into a union all operator

src/include/gpopt/translate/CTranslatorUtils.h
@@ -71,12 +71,14 @@ enum DistributionHashOpsKind
 class CTranslatorUtils
 {
 private:
-	// construct a set of column attnos corresponding to a single grouping set
+	// Construct a set of column attnos corresponding to a single grouping set
+	// from either a plain GROUP BY or one set in a list of grouping sets
 	static CBitSet *CreateAttnoSetForGroupingSet(CMemoryPool *mp,
 												 List *group_elems,
 												 ULONG num_cols,
 												 UlongToUlongMap *group_col_pos,
-												 CBitSet *group_cols);
+												 CBitSet *group_cols,
+												 bool use_group_clause);
 
 	// check if the given mdid array contains any of the polymorphic
 	// types (ANYELEMENT, ANYARRAY)
@@ -93,6 +95,16 @@ private:
 	static void UpdateGrpColMapping(CMemoryPool *mp,
 									UlongToUlongMap *grouping_col_to_pos_map,
 									CBitSet *group_cols, ULONG sort_group_ref);
+
+	// create a set of grouping sets for a rollup
+	static CBitSetArray *CreateGroupingSetsForRollup(
+		CMemoryPool *mp, const GroupingSet *grouping_set, ULONG num_cols,
+		CBitSet *group_cols, UlongToUlongMap *group_col_pos);
+
+	// create a set of grouping sets for a grouping sets subclause
+	static CBitSetArray *CreateGroupingSetsForSets(
+		CMemoryPool *mp, const GroupingSet *grouping_set_node, ULONG num_cols,
+		CBitSet *group_cols, UlongToUlongMap *group_col_pos);
 
 public:
 	struct SCmptypeStrategy
@@ -179,8 +191,8 @@ public:
 	// construct a dynamic array of sets of column attnos corresponding
 	// to the group by clause
 	static CBitSetArray *GetColumnAttnosForGroupBy(
-		CMemoryPool *mp, List *group_clause, ULONG num_cols,
-		UlongToUlongMap *group_col_pos, CBitSet *group_cold);
+		CMemoryPool *mp, List *group_clause, List *grouping_set_list,
+		ULONG num_cols, UlongToUlongMap *group_col_pos, CBitSet *group_cold);
 
 	// return a copy of the query with constant of unknown type being coerced
 	// to the common data type of the output target list

src/test/regress/expected/gporca.out
@@ -4349,6 +4349,14 @@ select a,1 from orca.r group by rollup(a);
     |        1
 (22 rows)
 
+select distinct grouping(a) + grouping(b) from orca.m group by rollup(a,b);
+ ?column? 
+----------
+        0
+        2
+        1
+(3 rows)
+
 -- arrays
 select array[array[a,b]], array[b] from orca.r;
    array    | array 
@@ -8727,7 +8735,7 @@ select max(a) from foo group by (select g from jazz where foo.a = (select max(a)
    3
 (2 rows)
 
--- group by inside groupby inside group by ;S
+-- group by inside groupby inside group by
 select max(a) from foo group by (select min(g) from jazz where foo.a = (select max(g) from jazz group by h) group by h);
  max 
 -----

src/test/regress/expected/gporca_optimizer.out
@@ -3726,8 +3726,6 @@ select * from orca.m where a=abs(b);
 
 -- grouping sets
 select a,b,count(*) from orca.m group by grouping sets ((a), (a,b));
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  a  | b  | count 
 ----+----+-------
   0 |  1 |     1
@@ -3806,8 +3804,6 @@ DETAIL:  Feature not supported: Grouping Sets
 (73 rows)
 
 select b,count(*) from orca.m group by grouping sets ((a), (a,b));
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  b  | count 
 ----+-------
   1 |     1
@@ -3886,8 +3882,6 @@ DETAIL:  Feature not supported: Grouping Sets
 (73 rows)
 
 select a,count(*) from orca.m group by grouping sets ((a), (a,b));
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  a  | count 
 ----+-------
   0 |     1
@@ -3966,8 +3960,6 @@ DETAIL:  Feature not supported: Grouping Sets
 (73 rows)
 
 select a,count(*) from orca.m group by grouping sets ((a), (b));
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  a  | count 
 ----+-------
     |     1
@@ -4012,8 +4004,6 @@ DETAIL:  Feature not supported: Grouping Sets
 (39 rows)
 
 select a,b,count(*) from orca.m group by rollup(a, b);
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  a  | b  | count 
 ----+----+-------
   0 |  1 |     1
@@ -4093,8 +4083,6 @@ DETAIL:  Feature not supported: Grouping Sets
 (74 rows)
 
 select a,b,count(*) from orca.m group by rollup((a),(a,b)) order by 1,2,3;
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  a  | b  | count 
 ----+----+-------
   0 |  1 |     1
@@ -4174,8 +4162,6 @@ DETAIL:  Feature not supported: Grouping Sets
 (74 rows)
 
 select count(*) from orca.m group by ();
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  count 
 -------
     38
@@ -4208,8 +4194,6 @@ select a, count(*) from orca.r group by (), a;
 (21 rows)
 
 select a, count(*) from orca.r group by grouping sets ((),(a));
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  a  | count 
 ----+-------
   2 |     1
@@ -4237,8 +4221,6 @@ DETAIL:  Feature not supported: Grouping Sets
 (22 rows)
 
 select a, b, count(*) c from orca.r group by grouping sets ((),(a), (a,b)) order by b,a,c;
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  a  | b | c  
 ----+---+----
   3 | 0 |  1
@@ -4287,8 +4269,6 @@ DETAIL:  Feature not supported: Grouping Sets
 (43 rows)
 
 select a, count(*) c from orca.r group by grouping sets ((),(a), (a,b)) order by b,a,c;
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  a  | c  
 ----+----
   3 |  1
@@ -4337,16 +4317,12 @@ DETAIL:  Feature not supported: Grouping Sets
 (43 rows)
 
 select 1 from orca.r group by ();
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  ?column? 
 ----------
         1
 (1 row)
 
 select a,1 from orca.r group by rollup(a);
-INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
  a  | ?column? 
 ----+----------
   1 |        1
@@ -4372,6 +4348,14 @@ DETAIL:  Feature not supported: Grouping Sets
  18 |        1
  20 |        1
 (22 rows)
+
+select distinct grouping(a) + grouping(b) from orca.m group by rollup(a,b);
+ ?column? 
+----------
+        0
+        1
+        2
+(3 rows)
 
 -- arrays
 select array[array[a,b]], array[b] from orca.r;
@@ -8951,7 +8935,7 @@ DETAIL:  DXL-to-PlStmt Translation: Assert not supported
    1
 (2 rows)
 
--- group by inside groupby inside group by ;S
+-- group by inside groupby inside group by
 select max(a) from foo group by (select min(g) from jazz where foo.a = (select max(g) from jazz group by h) group by h);
 INFO:  GPORCA failed to produce a plan, falling back to planner
 DETAIL:  DXL-to-PlStmt Translation: Assert not supported
@@ -10987,9 +10971,9 @@ set log_statement='none';
 set log_min_duration_statement=-1;
 set client_min_messages='log';
 explain select count(*) from foo group by cube(a,b);
-LOG:  2019-05-31 15:13:50:374760 PDT,THD000,NOTICE,"Feature not supported: Grouping Sets",
+LOG:  2020-10-19 17:17:37:192241 PDT,THD000,NOTICE,"Feature not supported: Cube",
 INFO:  GPORCA failed to produce a plan, falling back to planner
-DETAIL:  Feature not supported: Grouping Sets
+DETAIL:  Feature not supported: Cube
                                               QUERY PLAN                                              
 ------------------------------------------------------------------------------------------------------
  Gather Motion 3:1  (slice1; segments: 3)  (cost=1319.44..1496.29 rows=10611 width=16)

l-wang/gpdb

commit 7adf477063d3be381dfc7037fca9a2a742be1045

Fix memory leak in ORCA translator CreateGroupingSetsForRollup()
Jesse found this memory leak that was introduced in my previous commit
df3ce784285f7d9ceccfa761fd790ece65b65cde.

Adding some notes for how to detect ORCA memory leaks:

Just run ORCA MDP tests should detect memory leaks inside of gporca.
However, the translator code under gpopt is not tested by the MDP tests,
and we by default use GPDB memory contexts in the translator.

One way to detect memory leaks in the ORCA translator code is to set
"optimizer_use_gpdb_allocators=0" in the gpconfig files and run the
relevant queries. By doing that we instead use ORCA memory contexts and
ORCA will be able to detect the memory leaks.

Reported-by: Jesse Zhang <sbjesse@gmail.com>

src/backend/gpopt/translate/CTranslatorUtils.cpp
@@ -1182,6 +1182,7 @@ CTranslatorUtils::CreateGroupingSetsForRollup(CMemoryPool *mp,
 			mp, gs_current->content, num_cols, group_col_pos, group_cols,
 			false /* use_group_clause */);
 		current_result->Union(bset);
+		bset->Release();
 		col_attnos_arr->Append(GPOS_NEW(mp) CBitSet(mp, *current_result));
 	}
 	// add an empty set