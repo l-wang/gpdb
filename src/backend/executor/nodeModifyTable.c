167b1d8eff667ea6d2c5fb13c9c86c236d67ca73
Bhuvnesh Chaudhary <bchaudhary@vmware.com>
2021-04-11 23:54:11 -0700

    fixup! Don't fetch partition check expression during InitResultRelInfo.

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -1706,7 +1706,7 @@ ExecSplitUpdate_Insert(ModifyTableState *mtstate,
 	 * row.  So skip the WCO checks if the partition constraint fails.
 	 */
 	partition_constraint_failed =
-		resultRelInfo->ri_PartitionCheck &&
+		resultRelationDesc->rd_rel->relispartition &&
 		!ExecPartitionCheck(resultRelInfo, slot, estate, false);
 
 	if (!partition_constraint_failed &&
